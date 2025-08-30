835bfd5e53028a78fbbb37987baa2e915ff34fd8
Annas <annaskherunas06@gmail.com>
2025-08-30 20:42:36 +0700

    test github workflow

--- installers/linux-x86_64/installer_script.py
+++ installers/linux-x86_64/installer_script.py
@@ -8,8 +8,41 @@ if __name__ == "__main__":
     args = parser.parse_args()
     version = args.version
     
-    os.system(f'bash -c "fpm -s dir -t deb -p cmakeg-{version}-linux-x64-debian.deb --name cmakeg --license MIT --version {version} --architecture x86_64 --description "Program to generate new cmake projects" --url "https://github.com/annasajkh/cmakeg" --maintainer "AnnasVirtual" --after-install install_permissions.sh --after-remove <(echo "rm -rf /opt/cmakeg && rm -f /usr/bin/cmakeg") ../../build/linux-x86_64/bin/cmakeg/release/=/opt/cmakeg/"')
+    os.system(f'fpm -s dir -t deb \
+   -p cmakeg-{version}-linux-x64-debian.deb \
+   --name cmakeg \
+   --license MIT \
+   --version {version} \
+   --architecture x86_64 \
+   --description "Program to generate new cmake projects" \
+   --url "https://github.com/annasajkh/cmakeg" \
+   --maintainer "AnnasVirtual" \
+   --after-install install_permissions.sh \
+   --after-remove <(echo "rm -rf /opt/cmakeg && rm -f /usr/bin/cmakeg") \
+   build/linux-x86_64/bin/cmakeg/release/=/opt/cmakeg/')
     
-    os.system(f'bash -c "fpm -s dir -t rpm -p cmakeg-{version}-linux-x64-fedora.rpm --name cmakeg --license MIT --version {version} --architecture x86_64 --description "Program to generate new cmake projects" --url "https://github.com/annasajkh/cmakeg" --maintainer "AnnasVirtual" --after-install install_permissions.sh --after-remove <(echo "rm -rf /opt/cmakeg && rm -f /usr/bin/cmakeg") ../../build/linux-x86_64/bin/cmakeg/release/=/opt/cmakeg/"')
+    os.system(f'fpm -s dir -t rpm \
+   -p cmakeg-{version}-linux-x64-fedora.rpm \
+   --name cmakeg \
+   --license MIT \
+   --version {version} \
+   --architecture x86_64 \
+   --description "Program to generate new cmake projects" \
+   --url "https://github.com/annasajkh/cmakeg" \
+   --maintainer "AnnasVirtual" \
+   --after-install install_permissions.sh \
+   --after-remove <(echo "rm -rf /opt/cmakeg && rm -f /usr/bin/cmakeg") \
+   build/linux-x86_64/bin/cmakeg/release/=/opt/cmakeg/')
     
-    os.system(f'bash -c "fpm -s dir -t pacman -p cmakeg-{version}-linux-x64-arch.pkg.tar.zst --name cmakeg --license MIT --version {version} --architecture x86_64 --description "Program to generate new cmake projects" --url "https://github.com/annasajkh/cmakeg" --maintainer "AnnasVirtual" --after-install install_permissions.sh --after-remove <(echo "rm -rf /opt/cmakeg && rm -f /usr/bin/cmakeg") ../../build/linux-x86_64/bin/cmakeg/release/=/opt/cmakeg/"')
+    os.system(f'fpm -s dir -t pacman \
+   -p cmakeg-{version}-linux-x64-arch.pkg.tar.zst \
+   --name cmakeg \
+   --license MIT \
+   --version {version} \
+   --architecture x86_64 \
+   --description "Program to generate new cmake projects" \
+   --url "https://github.com/annasajkh/cmakeg" \
+   --maintainer "AnnasVirtual" \
+   --after-install install_permissions.sh \
+   --after-remove <(echo "rm -rf /opt/cmakeg && rm -f /usr/bin/cmakeg") \
+   build/linux-x86_64/bin/cmakeg/release/=/opt/cmakeg/')
