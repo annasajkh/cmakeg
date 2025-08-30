1737dd7b18c2422fc5d246d6467671208cc5622f
Annas <annaskherunas06@gmail.com>
2025-08-30 20:15:36 +0700

    test github workflow

--- actions_utils/upload_to_github_release.py
+++ actions_utils/upload_to_github_release.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     version = args.version
 
-    os.system(f'gh release create {version} --title "Released {version} --generate-notes')
+    os.system(f'gh release create {version} --title "Released {version}" --generate-notes')
 
     for filename in glob.glob("installers/*"):
         print(f"file: {filename}")
