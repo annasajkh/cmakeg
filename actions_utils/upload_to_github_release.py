d76705934143a0ce6f0d1bee769dd2408c5832ff
Annas <annaskherunas06@gmail.com>
2025-08-31 09:14:01 +0700

    fix --add-assets on mulitple project and make debug/release folder name lowercase

--- actions_utils/upload_to_github_release.py
+++ actions_utils/upload_to_github_release.py
@@ -10,17 +10,14 @@ if __name__ == "__main__":
 
     os.system(f'gh release create {version} --title "Released {version}" --generate-notes')
 
-    search_dirs = ["installers", "linux-installers", "windows-installer"]
+    search_dirs = ["linux-installers", "windows-installer"]
 
     for search_dir in search_dirs:
-        if not os.path.exists(search_dir):
-            continue
-
         for root, dirs, files in os.walk(search_dir):
             for file in files:
                 file_path = os.path.join(root, file)
 
                 if file.endswith((".exe", ".deb", ".rpm", ".pkg.tar.zst")):
                     print(f"uploading: {file} on {file_path}")
-                    
+
                     os.system(f'gh release upload {version} "{file_path}" --clobber')
