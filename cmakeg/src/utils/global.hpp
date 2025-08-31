a2b40f5583acdab459778450a2b70fda03263ef0
annasajkh <annaskherunas06@gmail.com>
2025-08-31 13:14:08 +0700

    --add-assets syncing

--- cmakeg/src/utils/global.hpp
+++ cmakeg/src/utils/global.hpp
@@ -6,10 +6,11 @@ namespace global
     const std::string assetsCMakeStr = R"(
 
 add_custom_target(ProjectName_copy_assets_dir
-	COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different 
-        "${CMAKE_CURRENT_SOURCE_DIR}/assets/"
+    COMMAND ${CMAKE_COMMAND} -E rm -rf "$<TARGET_FILE_DIR:${PROJECT_NAME}>/assets"
+    COMMAND ${CMAKE_COMMAND} -E copy_directory 
+        "${CMAKE_CURRENT_SOURCE_DIR}/assets"
         "$<TARGET_FILE_DIR:${PROJECT_NAME}>/assets"
-    COMMENT "Copying assets directory"
+    COMMENT "Syncing assets directory"
 )
 
 add_dependencies(${PROJECT_NAME} ProjectName_copy_assets_dir))";
